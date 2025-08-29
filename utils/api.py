65e3927cd20c50f7f6dd174e6919b3572b53b729
gabrielaquinosouza <gabrielaquinosouza>
2025-08-29 02:07:59 +0000

    Desenvolvimento de Notebook: Extração da Produção de Milho
    Foi Desenvolvido o notebook para a Extração da Produção de Milho, também foi criado o pacote utils para reutilização de  códigos

---
+++ utils/api.py
@@ -0,0 +1,19 @@
+# Pacote python com funções de chamada de API que serão reutilizadas em varios notebooks
+
+import requests
+from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_exception_type
+
+
+# Referencia retry + backof => https://medium.com/@bounouh.fedi/enhancing-resilience-in-python-applications-with-tenacity-a-comprehensive-guide-d92fe0e07d8
+
+# Função que chama a API com retry + backoff 
+# Em casos de exceções da biblioteca requests ocorrerar outras tentativas com intervalos de 2s, 4s, 8s, 16s e 30s
+@retry(
+    stop=stop_after_attempt(5),
+    wait=wait_exponential(multiplier=1, min=2, max=30),
+    retry=retry_if_exception_type((requests.exceptions.RequestException,))
+)
+def buscar_dados(url):
+    resposta = requests.get(url, timeout=30)
+    resposta.raise_for_status()
+    return resposta.json()
